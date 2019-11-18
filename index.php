c9ddcb81212e99ca14a2e633cb74d96194dc7b03
marecmabrki <49040784+marecmabrki@users.noreply.github.com>
2019-11-18 17:39:04 +0100

    action added on index.php

--- index.php
+++ index.php
@@ -2,14 +2,20 @@
 
 require('controller.php');
 
-if(isset($_POST['name'], $_POST['message'])) {
-    createPost();
-} elseif (isset($_GET['action'])) {
-    if ($_GET['action'] == 'addComment' && isset($_POST['author'], $_POST['comment'])) {
-        addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+if (isset($_GET['action'])) {
+    if($_GET['action'] == 'createPost') {
+        if(isset($_POST['name'], $_POST['message'])) {
+            createPost();
+        }
+    } elseif ($_GET['action'] == 'displayPost') {
+        if (isset($_GET['id'])) {
+            readPost($_GET['id']);
+        }
+    } elseif ($_GET['action'] == 'addComment') {
+        if (isset($_POST['author'], $_POST['comment'])) {
+            addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+        }
     }
-} elseif (isset($_GET['id'])) {
-    readPost($_GET['id']);
 } else {
     listPosts();
 }
